cd04cb39e635146b6f5ed49f9b089a578f9c5bef
Lisa Vinson <Lrv0828@gmail.com>
2020-06-02 22:08:37 -0400

    created portfolio component. 2 x 2 grid with header spanning both columns, image, and project text

---
+++ assets/files/skills.js
@@ -0,0 +1,71 @@
+const skills = [
+    {   
+        category: "design",
+        skill: "html",
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "design",
+        skill: "css",
+        details: ["layouts with floats, flexbox, or css grid","animations", "responsive design / media queries", "cross browser compatibility", "BEM methodology"]
+    }, {   
+        category: "design",
+        skill: "sass",
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "language",
+        skill: "javascript",
+        details: ["asynchronous programming", "prototypal inheritance", ]
+    }, {   
+        category: "database",
+        skill: "jquery",s
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "database",
+        skill: "react",
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "database",
+        skill: "node",
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "database",
+        skill: "c#",
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "database",
+        skill: "express",
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "database",
+        skill: "mongodb",
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "database",
+        skill: "mongoose",
+        details: ["semantic","constructing complex layouts"]
+    },
+    {   category: "database",
+        skill: "firebase",
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "database",
+        skill: "sql",
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "database",
+        skill: "sequelize",
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "database",
+        skill: "git",
+        details: ["semantic","constructing complex layouts"]
+    }, {   
+        category: "database",
+        skill: "figma",
+        details: ["semantic","constructing complex layouts"]
+    },  {   
+        category: "database",
+        skill: "mongoose",
+        details: ["semantic","constructing complex layouts"]
+    }
+]
