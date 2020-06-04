6c0c317abab107bf3734a2689ae36f26f30ec31b
Lisa Vinson <Lrv0828@gmail.com>
2020-06-03 20:56:57 -0400

    added skills section markup with buttons and details panels styled to sit side by side. Added jus functions to add click events to btns, and created click event function to retrieve skills information for clicked button and display to the dom

--- assets/files/skills.js
+++ assets/files/skills.js
@@ -16,56 +16,54 @@ const skills = [
         skill: "javascript",
         details: ["asynchronous programming", "prototypal inheritance", ]
     }, {   
-        category: "database",
-        skill: "jquery",s
-        details: ["semantic","constructing complex layouts"]
+        category: "language",
+        skill: "C#/.Net",
+        details: ["TBD", "TBD"]
     }, {   
-        category: "database",
+        category: "web framework",
         skill: "react",
-        details: ["semantic","constructing complex layouts"]
+        details: ["TBD", "TBD"]
     }, {   
-        category: "database",
+        category: "tbd",
         skill: "node",
-        details: ["semantic","constructing complex layouts"]
-    }, {   
-        category: "database",
-        skill: "c#",
-        details: ["semantic","constructing complex layouts"]
-    }, {   
-        category: "database",
+        details: ["TBD", "TBD"]
+    },  {   
+        category: "tbd",
         skill: "express",
-        details: ["semantic","constructing complex layouts"]
+        details: ["TBD", "TBD"]
     }, {   
         category: "database",
         skill: "mongodb",
-        details: ["semantic","constructing complex layouts"]
+        details: ["TBD", "TBD"]
     }, {   
         category: "database",
         skill: "mongoose",
-        details: ["semantic","constructing complex layouts"]
+        details: ["TBD", "TBD"]
     },
     {   category: "database",
         skill: "firebase",
-        details: ["semantic","constructing complex layouts"]
+        details: ["TBD", "TBD"]
     }, {   
         category: "database",
         skill: "sql",
-        details: ["semantic","constructing complex layouts"]
+        details: ["TBD", "TBD"]
     }, {   
         category: "database",
         skill: "sequelize",
-        details: ["semantic","constructing complex layouts"]
+        details: ["TBD", "TBD"]
     }, {   
-        category: "database",
+        category: "tools",
         skill: "git",
-        details: ["semantic","constructing complex layouts"]
+        details: ["TBD", "TBD"]
     }, {   
-        category: "database",
+        category: "tools",
         skill: "figma",
-        details: ["semantic","constructing complex layouts"]
-    },  {   
-        category: "database",
-        skill: "mongoose",
-        details: ["semantic","constructing complex layouts"]
-    }
+        details: ["TBD", "TBD"]
+    },  
 ]
+
+const data = {
+    skills
+}
+
+export default data;
---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+import data from "../files/skills.js";
+
+//Add click event to each 'skills' button
+window.addEventListener("load", function () {
+    console.log("loaded")
+    document.querySelectorAll(".btn-skills").forEach(item => {
+       
+        item.addEventListener('click', getSkillDetails)
+      })
+  })
+
+  //Click event: Retrieve details for selected skill
+const getSkillDetails = function() {
+    const skill = this.getAttribute("data-skill")
+    // console.log(skill)
+    const skillInfo = data.skills.find(item => item.skill === skill)
+    console.log(skillInfo);
+    displaySkillDetails(skillInfo)
+}
+
+//Display skill information to the dom
+const displaySkillDetails = ({ skill, details, category} ) => {  
+    //pickle - add error checking / checking for existing 
+    const paragraphs = document.querySelectorAll("div.skills__details > p")
+    console.log(paragraphs)
+    if(paragraphs.length) {
+        paragraphs.forEach(p => p.remove())
+    }
+    const heading = document.getElementsByClassName("skill__heading")[0]
+    console.log(heading);
+    heading.textContent = skill;
+    details.forEach(para => {
+        const paragraph = document.createElement("p");
+        const node = document.createTextNode(para);
+        paragraph.appendChild(node);
+        paragraph.setAttribute("class", "paragraph")
+        const element = document.getElementsByClassName("skills__details")[0];
+        console.log(paragraph);
+        console.log(element);
+        element.appendChild(paragraph);
+
+    })
+}
+
