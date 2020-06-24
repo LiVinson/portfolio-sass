18fa066c9065ed2d56b973eacbc10d54ab17c567
Lisa Vinson <Lrv0828@gmail.com>
2020-06-24 12:35:58 -0400

    moved contact route to own file for vercel deployment

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -21,58 +21,58 @@ app.get("/", (req, res) => {
     return res.sendFile(HTML_FILE)
 })
 
-//When user submits contact form, confirm data again, and send email
-app.post("/contact", confirmInput, sendEmail)
+// //When user submits contact form, confirm data again, and send email
+// app.post("/contact", confirmInput, sendEmail)
 
-//Temporary: Any 'other' routes - redirect back to the main page. Will add 404 later.
-app.get("*", (req,res) => {
-  res.redirect("/")
-})
+// //Temporary: Any 'other' routes - redirect back to the main page. Will add 404 later.
+// app.get("*", (req,res) => {
+//   res.redirect("/")
+// })
 
-//verifies that all form data is valid before calling sendEmail
-function confirmInput(req, res, next){
+// //verifies that all form data is valid before calling sendEmail
+// function confirmInput(req, res, next){
 
-  try {
-    const { name, email, message } = req.body
-    if(validateInput("name", name) && validateInput("email", email) && validateInput("message", message)){
-      next()
-    } else {
-      res.sendStatus(400)
-    }
-  } catch (error) {
-    res.sendStatus(error.responseCode || 500)    
-  } 
-}
+//   try {
+//     const { name, email, message } = req.body
+//     if(validateInput("name", name) && validateInput("email", email) && validateInput("message", message)){
+//       next()
+//     } else {
+//       res.sendStatus(400)
+//     }
+//   } catch (error) {
+//     res.sendStatus(error.responseCode || 500)    
+//   } 
+// }
 
 
-//Called if form input is valid. Gets email and pw from .env and sends form data w/ nodemailer
-async function sendEmail(req, res) {
-  try {
-    //Create an instance of transporter object
-    const { name, email, message } = req.body
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USERNAME, //
-        pass: process.env.EMAIL_PASSWORD, //
-      },
-    })
+// //Called if form input is valid. Gets email and pw from .env and sends form data w/ nodemailer
+// async function sendEmail(req, res) {
+//   try {
+//     //Create an instance of transporter object
+//     const { name, email, message } = req.body
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL_USERNAME, //
+//         pass: process.env.EMAIL_PASSWORD, //
+//       },
+//     })
 
-    const mailOptions = {
-      from: email, // sender address
-      to: process.env.EMAIL_USERNAME, // list of receivers
-      subject: `${name} visited your portfolio!`, // Subject line
-      text: email + " sent the following: \n" + message, // plain text body
-    }
+//     const mailOptions = {
+//       from: email, // sender address
+//       to: process.env.EMAIL_USERNAME, // list of receivers
+//       subject: `${name} visited your portfolio!`, // Subject line
+//       text: email + " sent the following: \n" + message, // plain text body
+//     }
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail(mailOptions)
-    res.sendStatus(200)
-  } catch (error) {
+//     // send mail with defined transport object
+//     let info = await transporter.sendMail(mailOptions)
+//     res.sendStatus(200)
+//   } catch (error) {
 
-    res.sendStatus(error.responseCode || 500)
-  }
-}
+//     res.sendStatus(error.responseCode || 500)
+//   }
+// }
 
 app.listen(port, () => { 
   console.log("mode: ", process.env.NODE_ENV)
